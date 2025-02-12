2c1773dde34517e341fac997c50df85fc2c81787
Ayomide9-code <ayotife009@gmail.com>
2025-02-12 13:29:50 -0800

    updated homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,7 +42,7 @@ function handleLogin(event) {
             <input
               v-model="email"
               type="email"
-              placeholder="Enter your email"
+              placeholder=""
               class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
               required
             />
@@ -52,7 +52,7 @@ function handleLogin(event) {
             <input
               v-model="password"
               type="password"
-              placeholder="Enter your password"
+              placeholder=""
               class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
               required
             />
